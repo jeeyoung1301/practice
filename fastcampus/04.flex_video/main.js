1ebe3e9f3f254e4a8bd0327f963892c012bb0564
jeeyoung1301 <jeeyoung1301@naver.com>
2022-03-05 19:56:00 +0900

    flexvideo

--- fastcampus/04.flex_video/main.js
+++ fastcampus/04.flex_video/main.js
@@ -1,29 +1,29 @@
-const articles = document.querySelectorAll('article');
-const aside = document.querySelector('aside');
-const btnClose = aside.querySelector('.btnClose');
+const articles = document.querySelectorAll("article");
+const aisde = document.querySelector("aside");
+const btnClose = aisde.querySelector(".btnClose");
 
-articles.forEach(article => {
-	article.addEventListener('mouseenter', e => {
-		e.currentTarget.querySelector('video').play();
-	})
+articles.forEach((article) => {
+	article.addEventListener("mouseover", (e) => {
+		e.currentTarget.querySelector("video").play();
+	});
+	article.addEventListener("mouseleave", (e) => {
+		e.currentTarget.querySelector("video").pause();
+	});
 
-	article.addEventListener('mouseleave', e => {
-		e.currentTarget.querySelector('video').pause();
-	})
+	article.addEventListener("click", (e) => {
+		const tit = e.currentTarget.querySelector("h2").innerText;
+		const con = e.currentTarget.querySelector("p").innerText;
+		const vidSrc = e.currentTarget
+			.querySelector("video")
+			.getAttribute("src");
 
-	article.addEventListener('click', e => {
-		const tit = e.currentTarget.querySelector('h2').innerText;
-		const con = e.currentTarget.querySelector('p').innerText;
-		const vidSrc = e.currentTarget.querySelector('video').getAttribute('src');
+		aisde.querySelector("h2").innerText = tit;
+		aisde.querySelector("p").innerText = con;
+		aisde.querySelector("video").setAttribute("src", vidSrc);
+		aisde.classList.add("on");
+	});
 
-		aside.querySelector('h2').innerText = tit;
-		aside.querySelector('p').innerText = con;
-		aside.querySelector('video').setAttribute('src', vidSrc);
-		aside.classList.add('on');
-	})
-
-	btnClose.addEventListener('click', ()=>{
-		aside.classList.remove('on');
-	})
-
-})
+	btnClose.addEventListener("click", () => {
+		aisde.classList.remove("on");
+	});
+});
