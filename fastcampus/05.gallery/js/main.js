75b9450598b70763ceb8ded4a0901fe2233d1368
jeeyoung1301 <jeeyoung1301@naver.com>
2022-03-14 17:02:39 +0900

    gallery

--- fastcampus/05.gallery/js/main.js
+++ fastcampus/05.gallery/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", () => {
+	//Isotope 실행 옵션
 	const grid = new Isotope("section", {
 		itemSelector: "article",
 		columWidth: "article",
@@ -6,17 +7,58 @@ window.addEventListener("load", () => {
 	});
 
 	const btns = document.querySelectorAll("main ul li");
-	btns.forEach((btn, index) => {
-		btn.addEventListener("click", (e) => {
+
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener("click", (e) => {
 			e.preventDefault();
 
-			for (let btn of btns) btn.classList.remove("on");
-			e.currentTarget.classList.add("on");
+			const isOn = e.currentTarget.classList.contains("on");
+			if (isOn) return;
 
-			const btn_a = e.currentTarget.querySelector("a");
-			const a_href = btn_a.getAttribute("href");
-
-			grid.arrange({ filter: a_href });
+			activation(e);
 		});
-	});
+	}
+
+	function activation(e) {
+		for (let j = 0; j < btns.length; j++) {
+			btns[j].classList.remove("on");
+		}
+		e.currentTarget.classList.add("on");
+
+		const btn_a = e.currentTarget.querySelector("a");
+		const a_href = btn_a.getAttribute("href");
+
+		grid.arrange({ filter: a_href });
+	}
+
+	//for문_필터링
+	// for (let i = 0; i < btns.length; i++) {
+	// 	btns[i].addEventListener("click", (e) => {
+	// 		e.preventDefault();
+	// 		for (let j = 0; j < btns.length; j++) {
+	// 			btns[j].classList.remove("on");
+	// 		}
+	// 		btns[i].classList.add("on");
+
+	// 		const btn_a = e.currentTarget.querySelector("a");
+	// 		const a_href = btn_a.getAttribute("href");
+
+	// 		grid.arrange({ filter: a_href });
+	// 	});
+	// }
+
+	//forEach문_필터링
+	// btns.forEach((btn, index) => {
+	// 	btn.addEventListener("click", (e) => {
+	// 		e.preventDefault();
+
+	// 		for (let btn of btns) btn.classList.remove("on");
+	// 		e.currentTarget.classList.add("on");
+
+	// 		const btn_a = e.currentTarget.querySelector("a");
+	// 		const a_href = btn_a.getAttribute("href");
+
+	// 		grid.arrange({ filter: a_href });
+	// 	});
+	// });
 });
